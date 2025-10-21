bfe34c21d14bc788a86a5a15353714b2d21e38d3
Straib <r.poesselt@googlemail.com>
2025-10-21 21:21:05 +0200

    test list Employees

--- src/parse.c
+++ src/parse.c
@@ -10,7 +10,7 @@
 #include "common.h"
 #include "parse.h"
 
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
+int list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
 {
     int i = 0;
     for(;i < dbhdr->count; i++) {
@@ -19,6 +19,8 @@ void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees)
         printf("\tAdress: %s\n", employees[i].address);
         printf("\tHours: %d\n", employees[i].hours);
     }
+
+    return STATUS_SUCCESS;
 }
 
 int add_employee(struct dbheader_t *dbhdr, struct employee_t **employees, char *addstring)
